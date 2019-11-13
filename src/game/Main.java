3d9d33615d060b3f4772a72501ac1e6983c8349f
oreotabetai <s17t213@stu.kagawa-u.ac.jp>
2019-11-13 17:15:33 +0900

    First commit

---
+++ src/game/Main.java
@@ -0,0 +1,28 @@
+package game;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	private static Stage stage = new Stage();
+	private static Scene scene;
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Scene.fxml"));
+			scene = new Scene(root, 400, 800);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/game/SceneController.java
@@ -0,0 +1,27 @@
+package game;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class SceneController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView ball;
+
+    @FXML
+    private AnchorPane base;
+
+    @FXML
+    void initialize() {
+
+    }
+}
