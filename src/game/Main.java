2ec9c175bf84fdb7c0b1be7e31b215c962b1969f
oreotabetai <s17t213@stu.kagawa-u.ac.jp>
2019-11-19 19:33:56 +0900

    Add keyPress method

--- src/game/Main.java
+++ src/game/Main.java
@@ -5,24 +5,39 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	private static Stage stage = new Stage();
 	public static Scene scene;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Scene.fxml"));
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("Scene.fxml"));
 			scene = new Scene(root, 400, 800);
 			stage.setScene(scene);
 			stage.show();
+
+			scene.setOnKeyPressed(e -> keyPressed(e));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+
 	public static void main(String[] args) {
 		launch(args);
+	}
+
+	private void keyPressed(KeyEvent e) {
+		switch (e.getCode()) {
+		case SPACE:
+			System.out.print("space");
+			break;
+		default:
+			break;
+		}
 	}
 }
--- src/game/SceneController.java
+++ src/game/SceneController.java
@@ -1,6 +1,6 @@
 package game;
 
-import javafx.animation.KeyFrame;
+
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -21,44 +21,11 @@ public class SceneController {
 	private Image image;
 	private Floor floor = new Floor();
 	public Ball ball = new Ball(ballImage);
-	private Timeline timer;
 
 	@FXML
 	void initialize() {
-		// System.out.print(base.getChildren().get(0));
 		image = floor.assignImage("normal");
 		base.getChildren().add(floor.generate(image, 0, 500, 13));
-
-		// Durationの感覚でhandleが呼び出される。
-		timer = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				//base.setOnKeyPressed(e -> System.out.println("キーが押されました。"));
-				base = floor.fallFloors(base);
-			}
-		}));
-		timer.setCycleCount(Timeline.INDEFINITE);
-		timer.play();
 	}
 
-	private void keyPressEvent(KeyEvent e, Ball ball) {
-		switch (e.getCode()) {
-		case LEFT:
-			ball.leftSide();
-			break;
-		case RIGHT:
-			ball.rightSide();
-			break;
-		case SPACE:
-			System.out.print("no");
-			if(!Ball.isAir) {
-				timer.stop();
-				base = floor.jump(base);
-				timer.play();
-			}
-			break;
-		default:
-			break;
-		}
-	}
 }
